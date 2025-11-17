de0c57ac38cccdfcf6cbe13f58cabbce0b692685
Abdullah <abdullahkhan221121@gmail.com>
2025-11-17 16:15:09 +0500

    first commit

---
+++ backend/server.mjs
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoutes from "./routes/authRoutes.mjs";
+
+dotenv.config();
+
+const port = process.env.PORT
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect Database
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_URL);
+  console.log("DB is connected")
+}
+
+app.get("/", (req, res) => {
+    res.send("LuxuryStay Backend Running...");
+});
+
+app.use("/api/auth", authRoutes);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
